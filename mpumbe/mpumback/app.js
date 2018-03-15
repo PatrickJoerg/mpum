9dd78aeb2d8ecfaa9ef51c7fa65c749d02044f2a
Patrick <patrick.joerg@gmail.com>
2018-03-14 18:45:03 -0600

    day2

--- mpumbe/mpumback/app.js
+++ mpumbe/mpumback/app.js
@@ -1,46 +1,54 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express        = require("express");
+const path           = require("path");
+const favicon        = require("serve-favicon");
+const logger         = require("morgan");
+const cookieParser   = require("cookie-parser");
+const bodyParser     = require("body-parser");
+const cors           = require("cors");
+const authController = require("./routes/authController");
+const session        = require("express-session");
+const passport       = require("passport");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const app            = express();
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Mongoose configuration
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/angular-authentication")
+  .then(console.log("Connected to DB!!"))
+
+// Session
+
+
+var corsOptions = {
+  origin: true,
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(session({
+  secret: "yogaimschloss",
+  resave: true,
+  saveUninitialized: true,
+  cookie: { httpOnly: true, maxAge: 2419200000 }
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// Passport configuration
+require("./config/passport")(passport,app);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use('/api', authController);
+app.all('/*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 module.exports = app;
